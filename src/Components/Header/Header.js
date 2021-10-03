f22974488e79f86fc80d07585402308a1f6b3f23
colefiscus <coletfiscus@gmail.com>
2021-10-03 12:55:26 -0600

    Add flexbox and Title component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,14 +4,30 @@ import styled from 'styled-components/macro';
 const Header = () => {
   return (
     <HeaderWrapper>
-      VIBECHECK
+      <Title>VIBECHECK</Title>
+      <Button>SIGN IN</Button>
+      <Button>HELP</Button>
     </HeaderWrapper>
   )
 }
 
 const HeaderWrapper = styled.header`
-  background-color: #FFD60A;
+  background-color: #FFDD00;
   width: 100%;
+  display: flex;
+`;
+
+const Title = styled.h1`
+  font-size: ${22/16}rem;
+`;
+
+const Button = styled.button`
+  width: 40x;
+  background-color: #C1292E;
+
+  &:first-of-type {
+    margin-left: auto;
+  }
 `;
 
 export default Header;
