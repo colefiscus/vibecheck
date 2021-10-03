382ca893b73c028808032acd917be8407d6fbc1c
colefiscus <coletfiscus@gmail.com>
2021-10-03 12:49:08 -0600

    Start on Header with styled components

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      VIBECHECK
+    </HeaderWrapper>
+  )
+}
+
+const HeaderWrapper = styled.header`
+  background-color: #FFD60A;
+  width: 100%;
+`;
+
+export default Header;
