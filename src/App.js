d7df5379aab376a4117b035429ed47e29de007bb
colefiscus <coletfiscus@gmail.com>
2021-10-10 12:59:18 -0600

    Add import for switch and route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import GlobalStyles from './GlobalStyles';
-import react
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div>
