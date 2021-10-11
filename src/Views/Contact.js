b5264cc85613b765e7b8ba70ff27ffa769a3be41
colefiscus <coletfiscus@gmail.com>
2021-10-11 11:55:43 -0600

    Add basic structure for Contact comp

---
+++ src/Views/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const Contact = () => {
+  return (
+    <main>
+      <h1>THIS IS Contact</h1>
+    </main>
+  )
+}
+
+export default Contact;
