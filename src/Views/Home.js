c20322334ca217fa18796802d84256f619b898f1
colefiscus <coletfiscus@gmail.com>
2021-10-11 11:55:19 -0600

    Add new views and create basic Home comp

---
+++ src/Views/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const Home = () => {
+  return (
+    <main>
+      <h1>THIS IS HOME</h1>
+    </main>
+  )
+}
+
+export default Home;
