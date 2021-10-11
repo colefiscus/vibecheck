daae93aa6fd3883dca992396d58a114217f05fca
colefiscus <coletfiscus@gmail.com>
2021-10-11 12:43:05 -0600

    Add import and route for Faqs to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Home from './Views/Home';
 import Contact from './Views/Contact';
+import Faqs from './Views/Faqs';
 import GlobalStyles from './GlobalStyles';
 import { Switch, Route } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home}></Route>
         <Route exact path="/contact" component={Contact}></Route>
+        <Route exact path="/faqs" component={Faqs}></Route>
       </Switch>
       <Footer />
       <GlobalStyles />
