47c6ca9e886ebb99c81d474a9f60f25b6f697c38
colefiscus <coletfiscus@gmail.com>
2021-10-13 12:22:57 -0600

    Delete index.css file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { BrowserRouter } from 'react-router-dom';
 
-const router = <BrowserRouter> <App /> </BrowserRouter>;
+const router = <BrowserRouter><App /></BrowserRouter>;
 
 ReactDOM.render( router, document.getElementById('root'));
 
