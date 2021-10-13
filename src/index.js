8d94d59cfaf577a28f211243f1aaa3b17d52ac69
colefiscus <coletfiscus@gmail.com>
2021-10-13 12:27:29 -0600

    Configure Auth0 comp

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-const router = <BrowserRouter><App /></BrowserRouter>;
+const router =  <Auth0Provider
+                  domain="dev-kyroorm8.us.auth0.com"
+                  clientId="jcj9edbcKafI6Xy2Uy3Uhi2Tvm5dF2n4"
+                  redirectUri={window.location.origin}
+                >
+                  <BrowserRouter>
+                    <App />
+                  </BrowserRouter>
+                </Auth0Provider>;
 
 ReactDOM.render( router, document.getElementById('root'));
 
