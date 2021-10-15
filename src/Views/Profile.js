a6305cb574d72a99c3491d6d56f4f2375fddb3ad
colefiscus <coletfiscus@gmail.com>
2021-10-15 15:57:51 -0600

    Add LogoutButton to Profile

--- src/Views/Profile.js
+++ src/Views/Profile.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components/macro";
 
+import LogoutButton from "../Components/Auth0/LogoutButton";
+
 const Profile = ({ user, isAuthenticated, isLoading }) => {
   return (
     <main>
-
+      <h2>THIS IS PROFILE</h2>
+      <LogoutButton>Log Out...</LogoutButton>
     </main>
   )
 }
