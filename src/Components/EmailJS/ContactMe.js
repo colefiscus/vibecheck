afa4a08757655c2f452dea8dca66afa1ff596cda
colefiscus <coletfiscus@gmail.com>
2021-10-19 15:27:17 -0600

    Add imports and bones for ContactMe

---
+++ src/Components/EmailJS/ContactMe.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { send } from 'emailjs-com';
+
+const ContactMe = () => {
+  return (
+    <form>
+      
+    </form>
+  )
+}
+
+export default ContactMe;
