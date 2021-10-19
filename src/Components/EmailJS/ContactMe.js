7536289737e4dfe285b7ca66223b23f6cb3765b6
colefiscus <coletfiscus@gmail.com>
2021-10-19 15:31:01 -0600

    Add inputs and button to ContactMe

--- src/Components/EmailJS/ContactMe.js
+++ src/Components/EmailJS/ContactMe.js
@@ -2,9 +2,36 @@ import React, { useState } from 'react';
 import { send } from 'emailjs-com';
 
 const ContactMe = () => {
+
+  
+
   return (
-    <form>
-      
+    <form onSubmit={onSubmit}>
+      <input 
+        name="from_name"
+        placeholder="from name"
+        value={toSend.from_name}
+        onChange={handleChange} 
+      />
+      <input 
+        name="to_name"
+        placeholder="to name"
+        value={toSend.to_name}
+        onChange={handleChange} 
+      />
+      <input 
+        name="message"
+        placeholder="Your message"
+        value={toSend.message}
+        onChange={handleChange} 
+      />
+      <input 
+        name="reply_to"
+        placeholder="Your email"
+        value={toSend.reply_to}
+        onChange={handleChange} 
+      />
+      <button type="submit">Submit</button>
     </form>
   )
 }
