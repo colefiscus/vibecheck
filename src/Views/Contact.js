baaf7e9390d3f89ac980bab1be1f1d5c13fcc674
colefiscus <coletfiscus@gmail.com>
2021-10-22 15:54:07 -0600

    Add GH logo to created SocialWrapper for links

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components/macro";
 import selfPortrait from "../Images/cole.jpeg";
+import ghLogo from "../Images/github-logo.png";
 
 import ContactMe from "../Components/EmailJS/ContactMe";
 
@@ -8,8 +9,13 @@ const Contact = () => {
   return (
     <ContactWrapper>
       <AboutWrapper style={{ "--alignment": "center" }}>
-        <h2 style={{ "marginBottom": 25 + "px", "borderBottom": 1 + "px solid"}}>About Cole Fiscus</h2>
-        <ImageWrapper align="right">
+        <SocialWrapper>
+          <h2>About Cole Fiscus • • •</h2>
+          <ImageWrapper style={{ "--size": 32 + "px" }}>
+            <Logo src={ghLogo} />
+          </ImageWrapper>
+        </SocialWrapper>
+        <ImageWrapper style={{ "--size": 300 + "px" }}>
           <Portrait src={selfPortrait} alt={"Portrait of the developer"} />
         </ImageWrapper>
         <p>The developer is a major foodie. He wanted a better place to store his ratings and reviews of restaurants than his Notion page or Yelp. He also came up with the rating system. He wanted something that could factor in the multiple aspects of the dining-out experience. The best restaurant that he's been to so far has been Sawyer, in Seattle, WA. Sawyer got a final score of 5.5. He hopes you like using ViibCheck. He appreciates that you're here. And he hopes to hear from you soon.</p>
@@ -33,10 +39,23 @@ const ContactWrapper = styled.main`
   padding: 30px;
 `;
 
+const SocialWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid;
+  line-height: 0;
+  margin-bottom: 25px;
+`;
+
+const Logo = styled.img`
+  width: 100%;
+`;
+
 const ImageWrapper = styled.div`
   align-self: center;
   margin: 16px;
-  width: 300px;
+  width: var(--size);
   border-radius: 50%;
   float: left;
 `;
