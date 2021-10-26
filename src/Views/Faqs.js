2e8d4a021ff063f45abe1c0a8cb323db831c9a20
colefiscus <coletfiscus@gmail.com>
2021-10-26 12:14:20 -0600

    Create title and flexbox for faqs

--- src/Views/Faqs.js
+++ src/Views/Faqs.js
@@ -4,13 +4,22 @@ import styled from "styled-components/macro";
 const Faqs = () => {
   return (
     <FaqsWrapper>
-      <h1>THIS IS FAQS</h1>
+      <Title>Frequently Asked Questions</Title>
     </FaqsWrapper>
   )
 }
 
 const FaqsWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+`;
 
+const Title = styled.h1`
+  font-family: 'Biryani';
+  font-size: ${18/14}rem;
 `;
 
 export default Faqs;
