199f6a7f25cb43ad2a6680797274387ea258e488
colefiscus <coletfiscus@gmail.com>
2021-10-26 13:02:53 -0600

    Create FAQ file with props and className for acc..

---
+++ src/Components/FAQs/FAQ.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FAQ = ({ faq, index }) => {
+  return (
+    <div
+      className={"open" + (faq.open ? "open" : "")}
+      key={index}
+    >
+      {faq.question}
+    </div>
+  )
+}
