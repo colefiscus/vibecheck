bea35e21086eed95de6721d881a14a608f00e9de
colefiscus <coletfiscus@gmail.com>
2021-11-12 14:00:05 -0700

    Add userRatings and restInfo to data

--- src/Data/sampleData.js
+++ src/Data/sampleData.js
@@ -1,5 +1,15 @@
 const data = {
-
+  userRatings: {
+    foodRating: 5,
+    vibeRating: 3,
+    serviceRating: 1,
+    valueRating: 6
+  },
+  restaurantInfo: {
+    name: "Aloha Island Grill",
+    city: "Spokane",
+    state: "WA",
+  }
 }
 
 export default data;
