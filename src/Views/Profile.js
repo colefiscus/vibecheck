01083b019dba1787d91fcdd75939c1330bdd0706
colefiscus <coletfiscus@gmail.com>
2022-04-20 15:18:51 -0600

    Add welcome message to Profile

--- src/Views/Profile.js
+++ src/Views/Profile.js
@@ -4,9 +4,12 @@ import styled from "styled-components/macro";
 import LogoutButton from "../Components/Auth0/LogoutButton";
 
 const Profile = ({ user, isAuthenticated }) => {
+
   return (
     isAuthenticated && <MainWrapper>
-        <h2>{user.given_name.toUpperCase()}'S PROFILE</h2>
+        <HomeWrapper>
+          <WelcomeHero>Hi {user.given_name}!</WelcomeHero>
+        </HomeWrapper>
         <ImageWrapper>
           <img src={user.picture} alt={user.name} style={{ "width": "100%" }} />
         </ImageWrapper>
@@ -22,6 +25,18 @@ const MainWrapper = styled.main`
   align-items: center;
   justify-content: center;
   min-height: 78%;
+`;
+
+const HomeWrapper = styled.main`
+  min-height: 78%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const WelcomeHero = styled.h1`
+  font-size: ${36/14}rem;
 `;
 
 const ImageWrapper = styled.div`
