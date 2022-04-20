686a4d60ce307607d7f58695fd436c8269975105
colefiscus <coletfiscus@gmail.com>
2022-04-20 16:32:33 -0600

    Create RestForm and RatingBox

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
+import data from "../../Data/sampleData";
 
 import NewRestForm from "../Components/NewRestForm/NewRestForm";
 
