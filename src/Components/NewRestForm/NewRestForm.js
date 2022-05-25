22184248491242e9380284523483445f29124f82
colefiscus <coletfiscus@gmail.com>
2022-05-25 12:16:29 -0600

    Add beginning of newRest form

--- src/Components/NewRestForm/NewRestForm.js
+++ src/Components/NewRestForm/NewRestForm.js
@@ -3,7 +3,17 @@ import styled from "styled-components/macro";
 
 const NewRestForm = () => {
   return (
-    <h2>Add New Restaurant</h2>
+    <>
+      <h2>Add New Restaurant</h2>
+      <form>
+        <label>Name:</label>
+        <input />
+        <label>Date Visited:</label>
+        <input type="date" />
+        <label>Website</label>
+        <input type="url" />
+      </form>
+    </>
   )
 }
 
